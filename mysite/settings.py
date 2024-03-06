6985b59c58bbcfb6303ee6f4c2a9396beee1bdc0
Mikiejoe <omoshjoe02@gmail.com>
2024-03-06 13:24:59 +0300

    update permissions

--- mysite/settings.py
+++ mysite/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.app'
 SITE_ID = 1
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOWED_ORIGINS = ['*']
 CORS_ALLOW_HEADERS = ['*']  # Allow all headers, including 'Authorization'
 
 
@@ -121,8 +121,8 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES':[
-        # 'rest_framework.permissions.AllowAny',
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
+        # 'rest_framework.permissions.IsAuthenticated',
     ],
 }
 
